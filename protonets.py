0044670af9c8e2337d49e639c41bf4c0cc41681f
d6ms <g.denden.ms@gmail.com>
2020-07-27 19:16:52 +0900

    implement dataloader loop

--- protonets.py
+++ protonets.py
@@ -23,21 +23,42 @@ def parse_args():
 def train():
     pass
 
+
 def predict():
     pass
 
+
 def test():
+    n, k, q = 1, 3, 5
     dataset = OmniglotDataset(subset='background')
     sampler = FewShotBatchSampler(dataset,
                                   episodes_per_epoch=100,
-                                  n=1,
-                                  k=2,
-                                  q=4,
+                                  n=n,
+                                  k=k,
+                                  q=q,
                                   num_tasks=1)
     dataloader = DataLoader(dataset, batch_sampler=sampler, num_workers=4)
-    for data, label in dataloader:
-        print(data.shape)
+    for i, batch in enumerate(dataloader):
+        # x.shape: [k * (n + q) * num_tasks, 1, 105, 105]
+        # y.shape: [k * q]
+        x, y = prepare_batch(batch, k, q)
         break
+
+
+def prepare_batch(batch, k, q):
+    # data.shape: [batch_size, channels, height, width] = [k * (n + q) * num_tasks, 1, 105, 105]
+    # label.shape: [k * (n + q) * num_tasks] 
+    # k * (n + q) means [n support sets of class 1 to k, q query sets of class 1 to k]
+    data, label = batch
+
+    x = data.double() # .cuda()
+
+    # y = [*([0] * q), *([1] * q), ..., *([k - 1] * q)]
+    # 正解ラベルはq個ごとにまとまっているため，q個ごとにclassification用のラベルを[0, k)で振り直す
+    y = torch.arange(0, k, 1 / q).long() # .cuda()
+
+    return x, y
+
 
 if __name__ == '__main__':
     args, parser = parse_args()
